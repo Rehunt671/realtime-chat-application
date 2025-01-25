d07168c0f4522fa9389f2a7cf670cf8b4f9eebe9
nono <nonokub.671@gmail.com>
2025-01-25 20:55:45 +0700

    refacor:make baseUrl point to env

--- frontend/libs/axios.ts
+++ frontend/libs/axios.ts
@@ -1,23 +1,15 @@
-// axiosClient.ts
-
 import axios from "axios";
 
-// Create an Axios instance with a base URL
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080", // Replace with your API's base URL
+  baseURL: `${process.env.API_BASE_URL}`, 
   headers: {
-    "Content-Type": "application/json", // Set content type if needed
-    // Add other global headers like Authorization here
-    // "Authorization": `Bearer ${token}`,
+    "Content-Type": "application/json", 
   },
-  timeout: 10000, // Optional: Set a timeout for requests (in ms)
+  timeout: 10000, 
 });
 
-// You can add request or response interceptors here if needed
 axiosClient.interceptors.request.use(
   (config) => {
-    // Modify the request before sending (e.g., adding token)
-    // Example: config.headers['Authorization'] = `Bearer ${token}`;
     return config;
   },
   (error) => {
@@ -27,10 +19,10 @@ axiosClient.interceptors.request.use(
 
 axiosClient.interceptors.response.use(
   (response) => {
-    return response; // Handle the response globally if needed
+    return response;
   },
   (error) => {
-    return Promise.reject(error); // Handle errors globally if needed
+    return Promise.reject(error); 
   }
 );
 
